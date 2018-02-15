6f44f25f8a9f54fa4acd0bcc38c7d49f0ba59e66
Rajat Jindal <rajatjindal83@gmail.com>
2018-02-14 22:42:14 -0800

    add genkeypair

---
+++ cmd/genKeypair.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/golang/glog"
+	"github.com/proofpoint/kubernetes-ldap/token"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+// genKeypairCmd represents the genKeypair command
+var genKeypairCmd = &cobra.Command{
+	Use:   "gen-keypair",
+	Short: "generate a new keypair for signing/verifying the token",
+	Run: func(cmd *cobra.Command, args []string) {
+		if _, err := os.Stat(keypairDir); os.IsNotExist(err) {
+			os.MkdirAll(keypairDir, 0700)
+		}
+
+		if err := token.GenerateKeypair(keypairDir); err != nil {
+			glog.Errorf("Error generating key pair: %v", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Generated keypair in %sq\n", keypairDir)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(genKeypairCmd)
+	genKeypairCmd.PersistentFlags().StringVar(&keypairDir, "keypair-dir", "keypair", "directory that contains keypair for signing/verifying tokens. Defaults to 'keypair'")
+}
--- cmd/serve.go
+++ cmd/serve.go
@@ -109,8 +109,7 @@ func init() {
 	RootCmd.Flags().BoolVar(&ldapUseInsecure, "use-insecure", false, "Disable LDAP TLS")
 
 	RootCmd.Flags().DurationVar(&tokenTtl, "token-ttl", 24*time.Hour, "TTL for the token")
-	RootCmd.Flags().StringVar(&keypairDir, "keypair-dir", "keypair", "directory that contains keypair for signing/verifying tokens. Defaults to 'keypair'")
-	RootCmd.Flags().BoolVar(&genKeypair, "gen-keypair", false, "generate new keypair")
+	RootCmd.Flags().BoolVar(&genKeypair, "gen-keypair", false, "generate new keypair while starting server")
 
 	viper.BindPFlags(RootCmd.Flags())
 	flag.CommandLine.Parse([]string{})
