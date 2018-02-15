4c8a3613591e17aa613293be207fa1babf0b3a19
Rajat Jindal <rajatjindal83@gmail.com>
2018-02-15 11:12:01 -0800

    make suggested changes

--- cmd/genKeypair.go
+++ cmd/genKeypair.go
@@ -13,13 +13,10 @@ var genKeypairCmd = &cobra.Command{
 	Use:   "gen-keypair",
 	Short: "generate a new keypair for signing/verifying the token",
 	Run: func(cmd *cobra.Command, args []string) {
-		if _, err := os.Stat(keypairDir); os.IsNotExist(err) {
-			os.MkdirAll(keypairDir, 0700)
-		}
+		os.MkdirAll(keypairDir, 0700)
 
 		if err := token.GenerateKeypair(keypairDir); err != nil {
-			glog.Errorf("Error generating key pair: %v", err)
-			os.Exit(1)
+			glog.Fatalf("Error generating key pair: %v", err)
 		}
 		fmt.Printf("Generated keypair in %s\n", keypairDir)
 	},
--- cmd/serve.go
+++ cmd/serve.go
@@ -91,7 +91,7 @@ func init() {
 		"",
 		"config file (default is $HOME/.kubernetes-ldap.yaml)")
 
-	RootCmd.PersistentFlags().StringVar(&keypairDir, "keypair-dir", "keypair", "directory that contains keypair for signing/verifying tokens. Defaults to 'keypair'")
+	RootCmd.PersistentFlags().StringVar(&keypairDir, "keypair-dir", "keypair", "directory that contains keypair for signing/verifying tokens.")
 
 	RootCmd.Flags().StringVar(&ldapHost, "ldap-host", "", "(Required Host or IP of the LDAP server )")
 	RootCmd.Flags().UintVar(&ldapPort, "ldap-port", 389, "LDAP server port")
